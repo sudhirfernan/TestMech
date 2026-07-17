06b2e816907ac2abea8780ac52e5b543defe162d
sudhirfernan <sudhirfernan@gmail.com>
2026-07-18 01:43:05 +0530

    stop tracking sit files

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { LoginDto } from './dto/login.dto';
-export declare class AuthService {
-    login(loginDto: LoginDto): {
-        message: string;
-        email: string;
-        password: string;
-    };
-}
